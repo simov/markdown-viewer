cf512eb7acf8be3ecb4149951d8e65e8fcca6ae9
simov <simeonvelichkov@gmail.com>
2015-07-17 18:18:56 +0300

    Migrate to space indentation

--- chrome/markdown.js
+++ chrome/markdown.js
@@ -1,35 +1,35 @@
 
 var md = (function () {
-    // marked
-    var defaults = {
-        gfm: true,
-        // highlight: null,
-        tables: true,
-        breaks: false,
-        pedantic: false,
-        sanitize: true,
-        smartLists: true,
-        smartypants: false,
-        langPrefix: 'language-'//prism
-    }
+  // marked
+  var defaults = {
+    gfm: true,
+    // highlight: null,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false,
+    langPrefix: 'language-'//prism
+  }
 
-    function compile (markdown, sendResponse) {
-        chrome.storage.sync.get(function (sync) {
-            marked.setOptions(sync.options)
+  function compile (markdown, sendResponse) {
+    chrome.storage.sync.get(function (sync) {
+      marked.setOptions(sync.options)
 
-            marked(markdown, function (err, html) {
-                if (err) throw err
-                // prism fix
-                html = html.replace(/language-html/g, 'language-markup')
-                html = html.replace(/language-js/g, 'language-javascript')
-                
-                sendResponse({message: 'marked', marked: html})
-            })
-        })
-    }
+      marked(markdown, function (err, html) {
+        if (err) throw err
+        // prism fix
+        html = html.replace(/language-html/g, 'language-markup')
+        html = html.replace(/language-js/g, 'language-javascript')
+        
+        sendResponse({message: 'marked', marked: html})
+      })
+    })
+  }
 
-    return {
-        defaults: defaults,
-        compile: compile
-    }
+  return {
+    defaults: defaults,
+    compile: compile
+  }
 }())
--- js/content.js
+++ js/content.js
@@ -1,69 +1,69 @@
 
 function injectCSS () {
-    var link = document.createElement('link')
-    link.rel = 'stylesheet'
-    link.type = 'text/css'
-    link.href = '#'
-    link.id = 'theme'
-    document.head.appendChild(link)
+  var link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.type = 'text/css'
+  link.href = '#'
+  link.id = 'theme'
+  document.head.appendChild(link)
 }
 
 $(function () {
-    injectCSS()
+  injectCSS()
+  
+  $('body').addClass('markdown-body')//github
+  $('pre').attr('id', 'markdown').hide()
+
+  chrome.extension.sendMessage({
+    message: 'markdown',
+    markdown: $('#markdown').text()
+  }, function (res) {
+    $('body').append('<div id="html">').find('#html').append(res.marked)
+    Prism.highlightAll()
+  })
+
+  chrome.extension.sendMessage({
+    message: 'settings',
+  }, function (data) {
+    $('#theme').attr('href', chrome.extension.getURL('/themes/'+data.theme+'.css'))
     
-    $('body').addClass('markdown-body')//github
-    $('pre').attr('id', 'markdown').hide()
+    $('#theme').attr('disabled', data.raw)
+    $('#markdown')[data.raw?'show':'hide']()
+    $('#html')[data.raw?'hide':'show']()
+  })
 
-    chrome.extension.sendMessage({
-        message: 'markdown',
-        markdown: $('#markdown').text()
-    }, function (res) {
-        $('body').append('<div id="html">').find('#html').append(res.marked)
-        Prism.highlightAll()
-    })
-
-    chrome.extension.sendMessage({
-        message: 'settings',
-    }, function (data) {
-        $('#theme').attr('href', chrome.extension.getURL('/themes/'+data.theme+'.css'))
-        
-        $('#theme').attr('disabled', data.raw)
-        $('#markdown')[data.raw?'show':'hide']()
-        $('#html')[data.raw?'hide':'show']()
-    })
-
-    $(window).on('load', function (e) {
-        setTimeout(function () {
-            var timeout = null
-            $(window).on('scroll', function (e) {
-                clearTimeout(timeout)
-                timeout = setTimeout(function () {
-                    localStorage.setItem('scrolltop', $(window).scrollTop())
-                }, 100)
-            })
-
-            $(window).scrollTop(localStorage.getItem('scrolltop'))
+  $(window).on('load', function (e) {
+    setTimeout(function () {
+      var timeout = null
+      $(window).on('scroll', function (e) {
+        clearTimeout(timeout)
+        timeout = setTimeout(function () {
+          localStorage.setItem('scrolltop', $(window).scrollTop())
         }, 100)
-    })
+      })
+
+      $(window).scrollTop(localStorage.getItem('scrolltop'))
+    }, 100)
+  })
 })
 
 chrome.extension.onMessage.addListener(function (req, sender, sendResponse) {
-    switch (req.message) {
-        case 'reload':
-            window.location.reload(true)
-            break
-        
-        case 'theme':
-            var raw = $('#theme').attr('disabled') == 'disabled'
-            $('#theme').remove()
-            injectCSS()
-            $('#theme').attr('href', chrome.extension.getURL('/themes/'+req.theme+'.css'))
-            $('#theme').attr('disabled', raw)
-            break
+  switch (req.message) {
+    case 'reload':
+      window.location.reload(true)
+      break
+    
+    case 'theme':
+      var raw = $('#theme').attr('disabled') == 'disabled'
+      $('#theme').remove()
+      injectCSS()
+      $('#theme').attr('href', chrome.extension.getURL('/themes/'+req.theme+'.css'))
+      $('#theme').attr('disabled', raw)
+      break
 
-        case 'raw':
-            $('#theme').attr('disabled', !($('#theme').attr('disabled') == 'disabled'))
-            $('#markdown, #html').toggle()
-            break
-    }
+    case 'raw':
+      $('#theme').attr('disabled', !($('#theme').attr('disabled') == 'disabled'))
+      $('#markdown, #html').toggle()
+      break
+  }
 })
