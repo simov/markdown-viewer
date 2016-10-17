37ca1f16abb2bbe1779f0b204f4ee88a2f0dd009
simov <simeonvelichkov@gmail.com>
2016-10-17 13:17:40 +0300

    Update options page

--- content/options.js
+++ content/options.js
@@ -9,7 +9,7 @@ var events = {
       origins: [document.querySelector('input').value]
     }, (granted) => {
       if (granted) {
-        all()
+        get()
         document.querySelector('input').value = ''
       }
     })
@@ -28,31 +28,45 @@ var events = {
   }
 }
 
-function all () {
+function get () {
   chrome.permissions.getAll((res) => {
     state.origins = res.origins
     m.redraw()
   })
 }
 
-m.mount(document.querySelector('body'), {
-  oninit: () => {
-    all()
-  },
+function oncreate (vnode) {
+  componentHandler.upgradeElements(vnode.dom)
+}
+
+get()
+
+m.mount(document.querySelector('main'), {
   view: () =>
-    m('div', [
-      m('h1', 'Permissions'),
-      m('p', 'Read and change your data on:'),
-      m('table', state.origins.map((origin) =>
-        m('tr', [
-          m('td', origin),
-          m('td', m('button', {onclick: events.remove(origin)}, 'Remove'))
+    m('.mdl-grid', [
+      m('.mdl-cell mdl-cell--8-col-tablet mdl-cell--12-col-desktop', [
+        m('h4', 'Add New Origin'),
+        m('form', [
+          m('input[placeholder="https://raw.githubusercontent.com"]'),
+          m('button[type=button].mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect',
+            {oncreate, onclick: events.add},
+            'Add')
         ])
-      )),
-      m('form', [
-        m('label', 'Origin'),
-        m('input[placeholder="https://raw.githubusercontent.com"]'),
-        m('button[type=button]', {onclick: events.add}, 'Add')
+      ]),
+      m('.mdl-cell mdl-cell--8-col-tablet mdl-cell--12-col-desktop', [
+        m('h4', 'Allowed Origins'),
+        m('table.mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp',
+          state.origins.map((origin) =>
+          m('tr', [
+            m('td.mdl-data-table__cell--non-numeric', origin),
+            m('td.mdl-data-table__cell--non-numeric',
+              m('button.mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon',
+                {oncreate, onclick: events.remove(origin), title: 'Remove'},
+                m('i.material-icons icon-remove')
+              )
+            )
+          ])
+        ))
       ])
     ])
 })
