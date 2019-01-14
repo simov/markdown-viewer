9c629931ad04b61d36dbdace2cd97be2c71b9ff7
simov <simeonvelichkov@gmail.com>
2019-01-14 11:13:15 +0200

    Add themes build script

---
+++ build/themes/themes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+npx csso --input ../markdown-themes/foghorn.css --output themes/foghorn.css
+npx csso --input ../markdown-themes/ghostwriter.css --output themes/ghostwriter.css
+npx csso --input ../markdown-themes/github.css --output themes/github.css
+npx csso --input ../markdown-themes/github-dark.css --output themes/github-dark.css
+npx csso --input ../markdown-themes/godspeed.css --output themes/godspeed.css
+npx csso --input ../markdown-themes/markdown5.css --output themes/markdown5.css
+npx csso --input ../markdown-themes/markdown6.css --output themes/markdown6.css
+npx csso --input ../markdown-themes/markdown7.css --output themes/markdown7.css
+npx csso --input ../markdown-themes/markdown8.css --output themes/markdown8.css
+npx csso --input ../markdown-themes/markdown9.css --output themes/markdown9.css
+npx csso --input ../markdown-themes/markdown-alt.css --output themes/markdown-alt.css
+npx csso --input ../markdown-themes/markdown.css --output themes/markdown.css
+npx csso --input ../markdown-themes/markedapp-byword.css --output themes/markedapp-byword.css
+npx csso --input ../markdown-themes/new-modern.css --output themes/new-modern.css
+npx csso --input ../markdown-themes/radar.css --output themes/radar.css
+npx csso --input ../markdown-themes/screen.css --output themes/screen.css
+npx csso --input ../markdown-themes/solarized-dark.css --output themes/solarized-dark.css
+npx csso --input ../markdown-themes/solarized-light.css --output themes/solarized-light.css
+npx csso --input ../markdown-themes/torpedo.css --output themes/torpedo.css
+npx csso --input ../markdown-themes/vostok.css --output themes/vostok.css
