375342fa245696a90622d37fe4c1aa6cdf5547f1
simov <simeonvelichkov@gmail.com>
2020-12-21 15:36:19 +0200

    Update build scripts

--- build/remark/index.js
+++ build/remark/index.js
@@ -5,8 +5,10 @@ module.exports = {
   parse: require('remark-parse'),
   stringify: require('remark-stringify'),
   // plugins
+  gfm: require('remark-gfm'),
   breaks: require('remark-breaks'),
   html: require('remark-html'),
   slug: require('remark-slug'),
+  footnotes: require('remark-footnotes'),
   frontmatter: require('remark-frontmatter'),
 }
