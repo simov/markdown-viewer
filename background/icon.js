ef8a8577b76ab6a7241e80e20b66a5374cb79c00
simov <simeonvelichkov@gmail.com>
2022-11-19 16:12:22 +0200

    Update icons

--- background/icon.js
+++ background/icon.js
@@ -1,28 +1,13 @@
 
 md.icon = ({storage: {state}}) => () => {
 
-  setTimeout(() => {
-    if (state.icon) {
-      chrome.action.setIcon({
-        path: {
-          '16' : '/icons/icon16-light.png',
-          '19' : '/icons/icon19-light.png',
-          '38' : '/icons/icon38-light.png',
-          '48' : '/icons/icon48-light.png',
-          '128' : '/icons/icon128-light.png'
-        }
-      })
-    }
-    else {
-      chrome.action.setIcon({
-        path: {
-          '16' : '/icons/icon16.png',
-          '19' : '/icons/icon19.png',
-          '38' : '/icons/icon38.png',
-          '48' : '/icons/icon48.png',
-          '128' : '/icons/icon128.png'
-        }
-      })
-    }
-  }, 100)
+  setTimeout((color) =>
+    chrome.action.setIcon({
+      path: [16, 19, 38, 48, 128].reduce((all, size) => (
+        color = state.icon ? 'light' : 'dark',
+        all[size] = `/icons/${color}/${size}x${size}.png`,
+        all
+      ), {})
+    })
+  , 100)
 }
