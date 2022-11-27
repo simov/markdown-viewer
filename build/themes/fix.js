bc069ce2739edee31cb745cc4dd89689f8ef40a2
simov <simeonvelichkov@gmail.com>
2022-11-27 09:34:38 +0200

    Theme fixes

--- build/themes/fix.js
+++ build/themes/fix.js
@@ -3,12 +3,24 @@ var fs = require('fs')
 var path = require('path')
 var themes = path.resolve(__dirname, '../../themes/')
 
-var css = fs.readFileSync(path.resolve(themes, 'mini.css'), 'utf8')
-
 fs.writeFileSync(
   path.resolve(themes, 'mini.css'),
-  css
+  fs.readFileSync(path.resolve(themes, 'mini.css'), 'utf8')
     .replace('*,h5', 'body')
     .replace('*,html', 'body'),
+  'utf8'
+)
+
+fs.writeFileSync(
+  path.resolve(themes, 'latex.css'),
+  fs.readFileSync(path.resolve(themes, 'latex.css'), 'utf8')
+    .replace('scroll-behavior:smooth', ''),
+  'utf8'
+)
+
+fs.writeFileSync(
+  path.resolve(themes, 'simple.css'),
+  fs.readFileSync(path.resolve(themes, 'simple.css'), 'utf8')
+    .replace('scroll-behavior:smooth', ''),
   'utf8'
 )
