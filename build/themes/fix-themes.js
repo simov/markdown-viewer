6949ecdd8d617d5cf0e0b87a9e003559496cc9c1
simov <simeonvelichkov@gmail.com>
2024-03-18 17:41:55 +0200

    Fix anchor icon missing on origins with strict csp

--- build/themes/fix-themes.js
+++ build/themes/fix-themes.js
@@ -4,6 +4,20 @@ var path = require('path')
 var themes = path.resolve(__dirname, '../../themes/')
 
 fs.writeFileSync(
+  path.resolve(themes, 'github.css'),
+  fs.readFileSync(path.resolve(themes, 'github.css'), 'utf8')
+    .replaceAll(/mask-image:url\(".*?"\)/g, 'mask-image:url("chrome-extension://__MSG_@@extension_id__/content/anchor.svg")'),
+  'utf8'
+)
+
+fs.writeFileSync(
+  path.resolve(themes, 'github-dark.css'),
+  fs.readFileSync(path.resolve(themes, 'github-dark.css'), 'utf8')
+    .replaceAll(/mask-image:url\(".*?"\)/g, 'mask-image:url("chrome-extension://__MSG_@@extension_id__/content/anchor.svg")'),
+  'utf8'
+)
+
+fs.writeFileSync(
   path.resolve(themes, 'mini.css'),
   fs.readFileSync(path.resolve(themes, 'mini.css'), 'utf8')
     .replace('*,h5', 'body')
